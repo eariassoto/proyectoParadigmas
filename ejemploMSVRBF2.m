3c5232022d42519a8182fb74a364a0ccf8ca208e
eariassoto <ariassotoemmanuel@gmail.com>
2015-09-06 17:48:34 -0600

    ejemplo listo

--- ejemploMSVRBF2.m
+++ ejemploMSVRBF2.m
@@ -16,7 +16,7 @@ La clase 0 esta representada por O\n \
 La clase 1 esta representada por +\n\n");
 
 % Carga los datos en las matrices X y y
-load('svmData2.mat');
+load('svmData3.mat');
 
 % Mostrar los datos de entrenamiento
 plotData(X, y);
@@ -30,7 +30,7 @@ fprintf('Entrenando MSV RBF.\n');
 % C muy grande: lower bias, high variance
 % C muy pequeño: high bias, lower variance
 C = 1;
-gamma = 100;
+gamma = 50;
 % implementar el gamma
 svmParam = cstrcat('-s 0 -t 2 -c ', num2str(C), ' -g ', num2str(gamma));
 model = svmtrain(y, X, svmParam);
@@ -45,7 +45,7 @@ fprintf('Datos de prediccion:\n');
 % Datos de prediccion
 
 test_label = [1;0;0;1]; % valores random o conocidos mx1
-Test_matrix = [0.1986 0.5644; 0.8523 0.6117; 0.3138 0.5697; 0.4382 0.8817]; % mxn
+Test_matrix = [-0.555 0.001; ; 0 -0.555; -0.1999 -0.444; -0.200 0.222]; % mxn
 
 for i = 1:size(Test_matrix, 1)
   fprintf('%f, %f\n', Test_matrix(i,1), Test_matrix(i,2));
