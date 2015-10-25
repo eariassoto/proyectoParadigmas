cd8a453ce7e0353761652adf6169f46440b463e1
eariassoto <ariassotoemmanuel@gmail.com>
2015-10-24 20:46:54 -0600

    adelanto 24/10

--- ejemploLeerMatOctave/ejemplo.m
+++ ejemploLeerMatOctave/ejemplo.m
@@ -1,2 +1,24 @@
-Matriz =  load ("-ascii", "matrizEjemplo.txt")
-save "matriz.mat" Matriz
+clear; 
+close all;
+clc
+
+[MatrizAtmosfericos, MatrizPrecipitacion] = obtenerMatrices();
+
+lim = fix(size(MatrizAtmosfericos, 1) * 0.7); % 70% de las tuplas
+
+AtmosfericosEntrenamiento = MatrizAtmosfericos([1:lim], :);
+AtmosfericosValidacion = MatrizAtmosfericos([(lim+1): end], :);
+
+precipitacionEntrenamiento = MatrizPrecipitacion([1:lim], 4); % la 4 col es la precip
+precipitacionValidacion = MatrizPrecipitacion([(lim+1): end], 4);
+
+
+C = 40;
+gamma = 100;
+% implementar el gamma
+svmParam = cstrcat('-s 3 -t 2 -c ', num2str(C), ' -g ', num2str(gamma));
+model = svmtrain(precipitacionEntrenamiento, AtmosfericosEntrenamiento, svmParam);
+
+precipitacionValidacion(1, :)
+
+svmpredict(precipitacionValidacion(1, :), AtmosfericosValidacion(1, :), model)
